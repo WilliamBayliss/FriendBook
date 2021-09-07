f66cc974e1f6688f617b1c947dbea8638ccfafdf
williambayliss <wmbayliss@gmail.com>
2021-09-07 19:23:59 -0400

    Update user model for omniauth-facebook/devise integration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  devise :omniauthable, :omniauth_providers => [:facebook]
+  
   has_many :posts
   has_many :comments
 
