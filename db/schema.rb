9451dffd1d32a469f38021d63160121920e2b7fb
williambayliss <wmbayliss@gmail.com>
2021-09-10 13:34:49 -0400

    db migrate after adding column to posts model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_07_225930) do
+ActiveRecord::Schema.define(version: 2021_09_10_172643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2021_09_07_225930) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.integer "likes"
+    t.string "content"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
