e6bf5bfe8d43bcc3447c6c6670b51b03b10cf9c6
williambayliss <wmbayliss@gmail.com>
2021-09-11 12:39:17 -0400

    migrate db after adding default value to likes for comments/posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_10_172643) do
+ActiveRecord::Schema.define(version: 2021_09_11_163727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2021_09_10_172643) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.bigint "post_id", null: false
-    t.integer "likes"
+    t.integer "likes", default: 0
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(version: 2021_09_10_172643) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
-    t.integer "likes"
+    t.integer "likes", default: 0
     t.string "content"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
