c5b3cf481610c2e9282ff80890644edbea42b4a7
williambayliss <wmbayliss@gmail.com>
2021-09-13 16:03:42 -0400

    Added content column to comments and migrated db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_13_185033) do
+ActiveRecord::Schema.define(version: 2021_09_13_200039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_09_13_185033) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.bigint "post_id", null: false
+    t.string "content"
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
