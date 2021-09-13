d10b3889210e6a98953eabd14ab4284f94c245b1
williambayliss <wmbayliss@gmail.com>
2021-09-13 16:06:38 -0400

    Create method builds a post based on first user, added like method to controller to create a like on call, create methhod will redirect to index on save

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,11 +22,12 @@ class PostsController < ApplicationController
 
   # POST /posts or /posts.json
   def create
-    @post = Post.new(post_params)
+    # TODO: change to current_user before deployment
+    @post = User.first.posts.build(post_params)
     @posts = Post.all
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: "Post was successfully created." }
+        format.html { render :index, notice: "Post was successfully created." }
         format.json { render :index, status: :created, location: @post }
       else
         format.html { render :index, status: :unprocessable_entity }
@@ -48,6 +49,13 @@ class PostsController < ApplicationController
     end
   end
 
+  def like
+    @post = Post.find(params[:id])
+    # TODO: Change to current_user.id before production
+    Like.create(user_id: User.first.id, likeable_type: Post, likeable_id: @post.id)
+    redirect_back(fallback_location: root_path)
+  end
+
   # DELETE /posts/1 or /posts/1.json
   def destroy
     @post.destroy
@@ -65,6 +73,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit{:content}
+      params.require(:post).permit(:content)
     end
 end
