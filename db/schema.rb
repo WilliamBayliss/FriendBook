7f47869ea2be260acf091dc6090b08245b032747
williambayliss <wmbayliss@gmail.com>
2021-09-13 14:58:09 -0400

    Create 'like' scaffold for allowing liking of posts/comments, create table for polymorphic association of likeable things, migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_13_184918) do
+ActiveRecord::Schema.define(version: 2021_09_13_185033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,16 @@ ActiveRecord::Schema.define(version: 2021_09_13_184918) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["friend_id"], name: "index_friendships_on_friend_id"
     t.index ["user_id"], name: "index_friendships_on_user_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.string "imageable_type"
+    t.bigint "imageable_id"
+    t.bigint "user_id_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["imageable_type", "imageable_id"], name: "index_likes_on_imageable"
+    t.index ["user_id_id"], name: "index_likes_on_user_id_id"
   end
 
   create_table "posts", force: :cascade do |t|
