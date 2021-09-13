f39c037ff3013ea67bf1c0a510b755b4c0337149
williambayliss <wmbayliss@gmail.com>
2021-09-13 14:50:03 -0400

    Deleted Likes column from comments and posts tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_11_163727) do
+ActiveRecord::Schema.define(version: 2021_09_13_184918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 2021_09_11_163727) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.bigint "post_id", null: false
-    t.integer "likes", default: 0
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
@@ -48,7 +47,6 @@ ActiveRecord::Schema.define(version: 2021_09_11_163727) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
-    t.integer "likes", default: 0
     t.string "content"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
