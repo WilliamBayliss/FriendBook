dd57ba62819904e7ab263c56f6479d10eb60f6f2
williambayliss <wmbayliss@gmail.com>
2021-10-12 12:18:23 -0400

    Migrate db after generating news feed model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_13_200039) do
+ActiveRecord::Schema.define(version: 2021_10_06_170134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,11 @@ ActiveRecord::Schema.define(version: 2021_09_13_200039) do
     t.string "content"
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "feeds", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "friend_requests", force: :cascade do |t|
