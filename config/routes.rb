09bcabc8542efafcd4b4026c4238fb9219429042
williambayliss <wmbayliss@gmail.com>
2021-10-12 12:18:06 -0400

    Added singular resource for news feed model and set root to show action

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: 'posts#index'
+  resources :feed
+  root to: 'feed#show'
 
   put '/post/:id/like', to: 'posts#like', as: 'like_post'
 end
